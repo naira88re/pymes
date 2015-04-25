1970f13eca8a0183114accc11bb71115cf13e938
Gabriel Delgado <gabitosoft@gmail.com>
2015-04-24 22:28:23 -0400

    cambios en archivo scriptJS para conectarse a servidor

--- web/HTMLpyme/public_html/js/scriptJS.js
+++ web/HTMLpyme/public_html/js/scriptJS.js
@@ -1,10 +1,30 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+function crearUsuario() {
 
+  var data = {
+  
+    'nombres': $('#nombres').val(),
+    'usuario': $('#login').val(),
+    'contrasena': $('#contrasena').val(),
+    'ci': $('#ci').val(),
+    'telefono': $('#telefono').val(),
+  };
+  console.log(data);
 
-function mostrarModal(){
-    $('#myModal').modal('show')
+  $.ajax({
+    url: "http://localhost/pymes/public/usuarios",
+    type: "POST",
+    dataType: 'json',
+    data: JSON.stringify(data),
+    processData: false,
+    contentType: 'application/json',
+    CrossDomain:true,
+    async: false,
+    success: function (data) {
+      console.log(data);
+    },
+    error: function (xhr, ajaxOptions, thrownError) { //Add these parameters to display the required response
+        console.log(xhr.status);
+        console.log(xhr.responseText);
+    }
+  });
 }
