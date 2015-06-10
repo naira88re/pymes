d580cb52587f094c9d0b7ec8eb9940132c525940
Gabriel Delgado <gabitosoft@gmail.com>
2015-06-09 22:51:05 -0400

    Se arreglaron los problemas con los items del menu

--- web/HTMLpyme/public_html/js/scriptJS.js
+++ web/HTMLpyme/public_html/js/scriptJS.js
@@ -1,21 +1,26 @@
 
 /**
  * Carga archivo html con los controles de la 
- * barra de navegacion y la pagina de autenticacion
+ * barra de navegacion y la pagina de autenticacion.
  * @returns 
  */
 function cargarPaginaPrincipal() {
   
   $(document).ready(function() {
+
     $(".navbar").load('navbar.html');
     $("#contenedor_principal").load('login.html');
   });
 }
 
-function cargarPagina(item) {
-  
-  //console.log($(item).parent('.dl-menu'));
-  console.log($(item).parent().find('#dl-menu'));
-  console.log($(item));
-  console.log($('#registrarProducto').parent());
+/**
+ * Permite a los menu items cargar determinadas paginas
+ * en el contenedor principal.
+ * @param {string} ruta
+ * @returns 
+ */
+function activarItem(ruta) {
+
+  $('#contenedor_principal').load(ruta);
+  $('#dl-menu').dlmenu('closeMenu');
 }
